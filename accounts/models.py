65f28049a1d39a2a7f67a64c20a305483621967b
Aashika <khatiwadaaashika12@gmail.com>
2026-05-12 17:54:55 +0545

    created a signal

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,6 @@ class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     bio=models.TextField(blank=True)
     created_at= models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return self.user.username
