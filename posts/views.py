5fd0751b7d4e87378c30c9b888f4ee3bf85530c7
Aashika <khatiwadaaashika12@gmail.com>
2026-05-12 12:42:25 +0545

    updated views in post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def createPost(request):
+    if request.method=="POST":
+        content= request.get.POST(content)
+
+        Post.objects.create(
+            user=request.user,
+            content=content
+        )
+        redirect('home')
+    redirect('home')
