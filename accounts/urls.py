3fed302b6d9591e7f2f56d0eaee0c82cb56f54fa
Aashika <khatiwadaaashika12@gmail.com>
2026-05-12 18:16:29 +0545

    updating templates with fiiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', views.loginUser,name="login"),
     path('logout', views.logoutUser,name="logout"),
     path('register', views.registerUser,name="register"),
+    path('profile/<str:username>/', views.profile, name="profile"),
 
 ]
