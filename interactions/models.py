1ba04add8031405041740cc522e527e1021ea775
Aashika <khatiwadaaashika12@gmail.com>
2026-05-13 13:00:29 +0545

    comments

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
 
 # Create your models here.
+class Comments(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
+    content=models.TextField()
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+       return f"{self.user.username} - {self.content[:20]}"
