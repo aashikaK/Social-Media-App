a4f92e77bd598693eaa439c4429f1e923f33a266
Aashika <khatiwadaaashika12@gmail.com>
2026-05-13 12:39:31 +0545

    deleting

--- interactions/views.py
+++ interactions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from posts.models import Post
 
 # Create your views here.
@@ -9,4 +10,6 @@ def deletePost(request,post_id):
     post= get_object_or_404(Post,id=post_id)
     if request.user == post.user:
         post.delete()
+        messages.success(request, "Post deleted successfully.")
+
     return redirect('home')
