a5d8f0ed6d92a5dd000c905b8edb95131db18394
Aashika <khatiwadaaashika12@gmail.com>
2026-05-13 13:14:52 +0545

    likes

--- interactions/urls.py
+++ interactions/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('delete/<int:post_id>/', views.deletePost, name="delete_post"),
+
+    path('like/<int:post_id>/', views.likePost, name="like_post"),
+    path('comment/<int:post_id>/', views.addComment, name="add_comment"),
 ]
--- interactions/views.py
+++ interactions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from posts.models import Post
+from .models import Like,Comment
 
 # Create your views here.
 
@@ -13,3 +14,27 @@ def deletePost(request,post_id):
         messages.success(request, "Post deleted successfully.")
 
     return redirect('home')
+
+@login_required
+def likePost(request,post_id):
+    post=get_object_or_404(Post,id=post_id) 
+    like,created=Like.objects.get_or_create(user=request.user,post=post)
+
+    if not created:
+        like.delete()
+
+    return redirect("home")
+
+@login_required
+def addComment(request,post_id):
+    post=get_object_or_404(Post,id=post_id)
+    content=request.POST.get('content')
+
+    if content:
+        Comment.objects.create(
+            user=request.user,
+            post=post,
+            content=content
+        )
+        messages.success(request, "Posted the comment successfully.")
+    return redirect("home")
