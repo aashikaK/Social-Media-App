8ba554630b4e3a32f09eb4036305762d98da8d8d
Aashika <khatiwadaaashika12@gmail.com>
2026-05-13 13:03:39 +0545

    likes

--- interactions/models.py
+++ interactions/models.py
@@ -4,7 +4,7 @@ from posts.models import Post
 
 
 # Create your models here.
-class Comments(models.Model):
+class Comment(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     post=models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
     content=models.TextField()
@@ -12,3 +12,12 @@ class Comments(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
        return f"{self.user.username} - {self.content[:20]}"
+    
+
+class Like(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE, related_name="likes")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
