d83755c36458c67a873e5cfdf85f1db096cfeecf
aashikaK <138367806+aashikaK@users.noreply.github.com>
2026-05-18 16:14:20 +0545

    Change SECRET_KEY in settings.py
    Updated the SECRET_KEY for the Django application.

--- socialhub/settings.py
+++ socialhub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*p*o*0xt21==$v)h8#2o)5*of6bqrgs7wom@9@c^to3tx$f!m'
+SECRET_KEY='om@9@c^to3tx$f!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
